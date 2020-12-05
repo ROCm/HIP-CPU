e234970c3b83bd6a99401222fab918e2858695f8
Alex Voicu <alexandru.voicu@amd.com>
2020-12-06 01:19:30 +0200

    Use homebaked vector types; remove SSM.

--- external/ssm/simd/types.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "access.hpp"
-#include "../detail/type-traits.hpp"
-
-#include "types/f128.inl"
-#include "types/s128.inl"
-#include "types/d128.inl"
-//#include "types/d256.inl"
